9c7758f6dc3dba53704a1f36fa27c3b5a4c18318
Grasseed <frank6873088@gmail.com>
2018-10-28 11:53:21 +0800

    下樓梯code

---
+++ Floor/Floor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("輸入階梯數:");
+            num = int.Parse(Console.ReadLine());
+            for(int k=1;k<=num;k++)
+            {
+                Console.WriteLine("_");
+                for(int i =1;i<=k;i++)
+                {    
+                    if(i==1)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");      
+                    }                               
+                }                
+                Console.Write("|");
+                if(k == num)
+                {
+                    Console.Write("_");
+                }
+            }            
+            Console.ReadKey();
+        }
+    }
+}
